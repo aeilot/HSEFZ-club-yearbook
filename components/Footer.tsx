266d9a7e82f9f6c1269d6e2a6d7a6d5bfd811825
Louis <1329415023@qq.com>
2023-07-23 18:27:15 +0800

    Optimize

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>华东师范大学第二附属中学版权所有</div>
 
       <div className={styles.settings}>
         {hasMounted && (
