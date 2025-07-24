09d57f91a2e28391488f17c428bce9482576b1a9
awa-easymoneysniper <bethany7690@hotmail.com>
2025-07-24 01:36:46 -0400

    add site

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '23467fb78c4580cc8c31e8e4b0345a77',
+  rootNotionPageId: '23a67fb78c4580b297e2e7ab51053382',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -53,13 +53,21 @@ export default siteConfig({
   // navigationStyle: 'default'
   navigationStyle: 'custom',
   navigationLinks: [
+    // {
+    //   title: '社团年鉴',
+    //   pageId: '23467fb78c4580cc8c31e8e4b0345a77'
+    // },
+    // {
+    //   title: '部门年鉴',
+    //   pageId: '23467fb78c4580508cc9de72453b832f'
+    // },
     {
       title: '分类',
-      pageId: '23467fb78c45810194abc9105f1cd449'
+      pageId: '23a67fb78c4581009994ef2fcb2177bd'
     },
     {
       title: '关于本站',
-      pageId: '23467fb78c458108b2c5e0c8fea4c6a5'
+      pageId: '23a67fb78c4581e89712dfe01b6193ab'
     }
   ]
 })
